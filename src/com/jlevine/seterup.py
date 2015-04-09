31b7c7fae87bb42ca8e31baa27ef94081630e21c
Justin Levine <jclevine.receipts@gmail.com>
2015-04-09 03:06:34 -0400

    Do not add new lists if they already exist.

--- src/com/jlevine/seterup.py
+++ src/com/jlevine/seterup.py
@@ -22,10 +22,19 @@ if __name__ == '__main__':
 
     dumb_lists = ['All Tasks', 'Personal', 'Study', 'Work']
     all_lists = my_rtm.lists.getList()
+    all_list_name_set = {rtm_list.name for rtm_list in all_lists.lists.list}
     dumb_list_ids = [dumb_list.id for dumb_list in all_lists.lists.list if dumb_list.name in dumb_lists]
 
     for dumb_list_id in dumb_list_ids:
         my_rtm.lists.delete(timeline=time.timeline, list_id=dumb_list_id)
 
+    # smart_list_ids = [smart_smartlist.id for smart_smartlist in all_lists.lists.list if dumb_list.name in dumb_lists]
+
+    # [my_rtm.lists.add(timeline=time.timeline, **smart_smartlist)  # Add list
+    #  for smart_smartlist in smart_smartlists                      # From all lists of to-add smart lists
+    #  if smart_smartlist.name not in all_list_name_set]            # If they don't already exist.
+
     for smart_smartlist in smart_smartlists:
-        my_rtm.lists.add(timeline=time.timeline, **smart_smartlist)
+        does_list_already_exist = smart_smartlist.name in all_list_name_set
+        if not does_list_already_exist:
+            my_rtm.lists.add(timeline=time.timeline, **smart_smartlist)
